31c0bdbedf451df059fe22d7ae01fec0b08affef
unknown <adil.photon@gmail.com>
2012-10-06 13:51:07 +0100

    Started work on navigation class structure.

---
+++ Application/Communication/Messages/Packets/Clientside/Navigator/PublicRooms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mango.Communication.Sessions;
+using Revolution.Core;
+using Revolution.Messages;
+
+namespace Revolution.Application.Communication.Messages.Packets.Clientside.Navigator
+{
+    class PublicRooms : IPacketEvent
+    {
+        public uint EventId
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void ParsePacket(Session session, Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
